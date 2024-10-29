e50a66b3648444bea725a2dd51c5083987f18995
kumawatshivani192001 <154692233+shivanikumawat19@users.noreply.github.com>
2024-10-29 17:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def main():
             print("Exiting the program.")
             break
         else:
-            print("Invalid choice.")
+            print("Invalid choiceee..")
             continue
 
         # Display the result
